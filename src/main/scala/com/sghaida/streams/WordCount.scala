788326972ff22f99d6663853bcb09e2a87132ff0
Saddam Abu Ghiada <saddam.abughaida@gmail.com>
2019-03-11 23:50:46 +0100

    organize

--- src/main/scala/com/sghaida/streams/WordCount.scala
+++ src/main/scala/com/sghaida/streams/WordCount.scala
@@ -18,33 +18,35 @@ object WordCount {
   private final val logger = LoggerFactory.getLogger("WordCount")
   implicit final def fromFiniteDuration(d: Duration): time.Duration = time.Duration.ofMillis(d.toMillis)
 
+  private val config = {
+
+    val props = new Properties()
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
+    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer.getClass.getName)
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG , "earliest")
+
+    props
+  }
+
+  val builder: StreamsBuilder = new StreamsBuilder
+  val textLines: KStream[String, String] = builder.stream[String, String]("streams-plaintext-input")
+  val wordCounts: KTable[String, Long] = textLines
+    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+    .groupBy((_, word) => word)
+    .count()
+
+  wordCounts.toStream.to("streams-wordcount-output")
+
   def main(args: Array[String]): Unit = {
-    val config = {
-
-      val props = new Properties()
-      props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-      props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
-      props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-      props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer.getClass.getName)
-      props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG , "earliest")
-
-      props
-    }
-
-    val builder: StreamsBuilder = new StreamsBuilder
-    val textLines: KStream[String, String] = builder.stream[String, String]("streams-plaintext-input")
-    val wordCounts: KTable[String, Long] = textLines
-      .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
-      .groupBy((_, word) => word)
-      .count()
-
-    wordCounts.toStream.to("streams-wordcount-output")
 
     val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
 
     streams.start()
 
     sys.ShutdownHookThread {
+      logger.info("shutdown received")
       streams.close(10 seconds)
     }
   }
